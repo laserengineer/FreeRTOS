c0d70a97a97ab3bd1723874f7ff8df3eb4a9e9e9
Yang Yu <yang.yu@nvent.com>
2025-07-24 16:05:07 -0600

    Ini Repo with mutiple color Blinking

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,33 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
-
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  // No need to initialize the RGB LED
+  Serial.begin(115200);
 }
 
+// the loop function runs over and over again forever
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+#ifdef RGB_BUILTIN
+  neopixelWrite(RGB_BUILTIN,RGB_BRIGHTNESS,RGB_BRIGHTNESS,RGB_BRIGHTNESS); // White
+  Serial.println("White");
+  delay(1000);
+  neopixelWrite(RGB_BUILTIN,0,RGB_BRIGHTNESS,RGB_BRIGHTNESS); // Cyan
+  Serial.println("Cyan");
+  delay(1000);
+  neopixelWrite(RGB_BUILTIN,RGB_BRIGHTNESS,RGB_BRIGHTNESS,0); // Yellow
+  Serial.println("Yellow");
+  delay(1000);
+  neopixelWrite(RGB_BUILTIN,RGB_BRIGHTNESS,0,0); // Red
+  Serial.println("Red");
+  delay(1000);
+  neopixelWrite(RGB_BUILTIN,0,RGB_BRIGHTNESS,0); // Green
+  Serial.println("Green");
+  delay(1000);
+  neopixelWrite(RGB_BUILTIN,0,0,RGB_BRIGHTNESS); // Blue
+  Serial.println("Blue");
+  delay(1000);
+  neopixelWrite(RGB_BUILTIN,0,0,0); // Off / black
+  Serial.println("Off");
+  delay(1000);
+#endif
 }
