b00ceca52350be75278dea1c7d2d16c329085118
Yang Yu <yy19@ualberta.ca>
2025-10-20 20:40:44 -0600

    work on lecture 3

--- src/main.cpp
+++ src/main.cpp
@@ -1,48 +1,53 @@
 #include <Arduino.h>
 
-void setup() {
-  // No need to initialize the RGB LED
-  Serial.begin(115200);
+int count1 = 0;
+int count2 = 0;
+#define LED_BUILTIN 2
+
+void task1(void *parameters)
+{
+    for (;;)
+    {                                    // infinite loop
+        digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage level)
+        Serial.print("Task 1 counter: ");
+        Serial.println(count1++);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
 }
 
-// the loop function runs over and over again forever
-void loop() {
-#ifdef RGB_BUILTIN
-  neopixelWrite(RGB_BUILTIN,RGB_BRIGHTNESS,RGB_BRIGHTNESS,RGB_BRIGHTNESS); // White
-  Serial.println("White");
-  delay(100);
-  neopixelWrite(RGB_BUILTIN,0,0,0); // Off / black
-  Serial.println("Off");
-  delay(100);
-  neopixelWrite(RGB_BUILTIN,0,RGB_BRIGHTNESS,RGB_BRIGHTNESS); // Cyan
-  Serial.println("Cyan");
-  delay(100);
-  neopixelWrite(RGB_BUILTIN,0,0,0); // Off / black
-  Serial.println("Off");
-  delay(100);
-  neopixelWrite(RGB_BUILTIN,RGB_BRIGHTNESS,RGB_BRIGHTNESS,0); // Yellow
-  Serial.println("Yellow");
-  delay(100);
-  neopixelWrite(RGB_BUILTIN,0,0,0); // Off / black
-  Serial.println("Off");
-  delay(100);
-  neopixelWrite(RGB_BUILTIN,RGB_BRIGHTNESS,0,0); // Red
-  Serial.println("Red");
-  delay(100);
-  neopixelWrite(RGB_BUILTIN,0,0,0); // Off / black
-  Serial.println("Off");
-  delay(100);
-  neopixelWrite(RGB_BUILTIN,0,RGB_BRIGHTNESS,0); // Green
-  Serial.println("Green");
-  delay(100);
-  neopixelWrite(RGB_BUILTIN,0,0,0); // Off / black
-  Serial.println("Off");
-  delay(100);
-  neopixelWrite(RGB_BUILTIN,0,0,RGB_BRIGHTNESS); // Blue
-  Serial.println("Blue");
-  delay(100);
-  neopixelWrite(RGB_BUILTIN,0,0,0); // Off / black
-  Serial.println("Off");
-  delay(100);
-#endif
+void task2(void *parameters)
+{
+    for (;;)
+    { // infinite loop
+        Serial.print("Task 2 counter: ");
+        Serial.println(count2++);
+        vTaskDelay(500 / portTICK_PERIOD_MS);
+        digitalWrite(LED_BUILTIN, LOW);
+        vTaskDelay(500 / portTICK_PERIOD_MS);
+    }
 }
+
+void setup()
+{
+    Serial.begin(115200);
+    pinMode(LED_BUILTIN, OUTPUT);
+    xTaskCreate(
+        task1,    // Function that should be called
+        "Task 1", // Name of the task (for debugging)
+        1000,     // Stack size (bytes)
+        NULL,     // Parameter to pass
+        1,        // Task priority
+        NULL      // Task handle
+    );
+
+    xTaskCreate(
+        task2,    // Function that should be called
+        "Task 2", // Name of the task (for debugging)
+        1000,     // Stack size (bytes)
+        NULL,     // Parameter to pass
+        1,        // Task priority
+        NULL      // Task handle
+    );
+}
+
+void loop() {}
--- test/main_RGB.cpp
+++ test/main_RGB.cpp
@@ -10,39 +10,39 @@ void loop() {
 #ifdef RGB_BUILTIN
   neopixelWrite(RGB_BUILTIN,RGB_BRIGHTNESS,RGB_BRIGHTNESS,RGB_BRIGHTNESS); // White
   Serial.println("White");
-  delay(100);
+  delay(1000);
   neopixelWrite(RGB_BUILTIN,0,0,0); // Off / black
   Serial.println("Off");
-  delay(100);
+  delay(1000);
   neopixelWrite(RGB_BUILTIN,0,RGB_BRIGHTNESS,RGB_BRIGHTNESS); // Cyan
   Serial.println("Cyan");
-  delay(100);
+  delay(1000);
   neopixelWrite(RGB_BUILTIN,0,0,0); // Off / black
   Serial.println("Off");
-  delay(100);
+  delay(1000);
   neopixelWrite(RGB_BUILTIN,RGB_BRIGHTNESS,RGB_BRIGHTNESS,0); // Yellow
   Serial.println("Yellow");
-  delay(100);
+  delay(1000);
   neopixelWrite(RGB_BUILTIN,0,0,0); // Off / black
   Serial.println("Off");
-  delay(100);
+  delay(1000);
   neopixelWrite(RGB_BUILTIN,RGB_BRIGHTNESS,0,0); // Red
   Serial.println("Red");
-  delay(100);
+  delay(1000);
   neopixelWrite(RGB_BUILTIN,0,0,0); // Off / black
   Serial.println("Off");
-  delay(100);
+  delay(1000);
   neopixelWrite(RGB_BUILTIN,0,RGB_BRIGHTNESS,0); // Green
   Serial.println("Green");
-  delay(100);
+  delay(1000);
   neopixelWrite(RGB_BUILTIN,0,0,0); // Off / black
   Serial.println("Off");
-  delay(100);
+  delay(1000);
   neopixelWrite(RGB_BUILTIN,0,0,RGB_BRIGHTNESS); // Blue
   Serial.println("Blue");
-  delay(100);
+  delay(1000);
   neopixelWrite(RGB_BUILTIN,0,0,0); // Off / black
   Serial.println("Off");
-  delay(100);
+  delay(1000);
 #endif
 }
