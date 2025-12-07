a86ce5366b68f86ad1a54d3eb2c6620435cc69ad
Yang Yu <yy19@ualberta.ca>
2025-12-07 16:04:22 -0700

    Finish mutex part

--- progress/part6_mutex_demo.cpp
+++ progress/part6_mutex_demo.cpp
@@ -1,6 +1,7 @@
-#include semphr.h
+#include <Arduino.h>
 
 // ---- Configuration ----
+// Use only core 1 for demo purposes
 #if CONFIG_FREERTOS_UNICORE
 static const BaseType_t app_cpu = 0;
 #else
@@ -9,37 +10,35 @@ static const BaseType_t app_cpu = 1;
 
 // Globals: Mutex for protecting Serial access
 static int shared_var = 0; // Example shared variable
-static semaphoreHandle_t mutex;
+static SemaphoreHandle_t mutex = nullptr;
+
 //*****************************************************************************
 
 // Task: Mutex Demo
-// Increment shared variable (the wrong way, without mutex protection)
-
+// Increment shared variable, with mutex protection
 void incTask(void *parameters)
 {
-    int local_var;
-
-    // Loop forever
     while (1)
     {
         // Roundabout way to do "Shared_var++" ramdomly and poorly
-        if (xSemaphoreTake(mutex, 0) == pdTrue)
+        if (xSemaphoreTake(mutex, 0) == pdTRUE)
         {
-            local_var = shared_var;
-            local_var++;
-            vTaskDelay(random(100, 500) / portTICK_PERIOD_MS);
-            shared_var = local_var;
+            // Critical section: safely increase shared_var
+            shared_var++;
+            Serial.print(pcTaskGetName(NULL));
+            Serial.print(" : ");
+            Serial.println(shared_var);
 
+            // Release the mutex
             xSemaphoreGive(mutex);
 
+            //Simulate variable task timing
+            vTaskDelay(random(100, 500) / portTICK_PERIOD_MS);
+
             // Print the value of shared_var);
-            Serial.println(shared_var);
+            taskYIELD();
         }
-        else
-        {
-            // Could not obtain the mutex
-            Serial.println("Could not obtain mutex");
-        }
+
     }
 }
 
@@ -54,15 +53,22 @@ void setup()
     // Create the mutex
     mutex = xSemaphoreCreateMutex();
 
+    if (mutex == nullprt)
+    {
+        Serial.println("Failed to create mutex");
+        while (1)
+            ;
+    }
+
     // Wait a moment to start
 
     vTaskDelay(1000 / portTICK_PERIOD_MS);
+
     Serial.println();
     Serial.println("--- FreeRTOS Race Condition Demo ---");
-    Serial.println("Incrementing a shared variable without mutex protection.");
+    Serial.println("Incrementing a shared variable with mutex protection.");
 
     // Create mutex-protected section
-    mutex = xSemaphoreCreateMutex();
 
     // Start the increment task
     xTaskCreatePinnedToCore(
