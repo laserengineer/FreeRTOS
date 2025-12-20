a7510c0544473ccd3e3a267f80beb51d5933ab81
Yang Yu <yy19@ualberta.ca>
2025-12-20 15:09:46 -0700

    Adjust semaphore

--- progress/part7_semaphore.cpp
+++ progress/part7_semaphore.cpp
@@ -1,20 +1,5 @@
-/**
- * FreeRTOS Counting Semaphore Challenge
- *
- * Challenge: use a mutex and counting semaphores to protect the shared buffer
- * so that each number (0 throguh 4) is printed exactly 3 times to the Serial
- * monitor (in any order). Do not use queues to do this!
- *
- * Hint: you will need 2 counting semaphores in addition to the mutex, one for
- * remembering number of filled slots in the buffer and another for
- * remembering the number of empty slots in the buffer.
- *
- * Date: January 24, 2021
- * Author: Shawn Hymel
- * License: 0BSD
- **/
-
 #include <Arduino.h>
+#include <stdint.h>
 
 #if CONFIG_FREERTOS_UNICORE
 static const BaseType_t app_cpu = 0;
@@ -22,7 +7,6 @@ static const BaseType_t app_cpu = 0;
 static const BaseType_t app_cpu = 1;
 #endif
 
-// Settings
 enum
 {
     BUF_SIZE = 5
@@ -31,39 +15,31 @@ static const int num_prod_tasks = 5;
 static const int num_cons_tasks = 2;
 static const int num_writes = 3;
 
-// Globals
 static int buf[BUF_SIZE];
-static int head = 0;
-static int tail = 0;
+static int write_index = 0;
+static int read_index = 0;
 static SemaphoreHandle_t bin_sem;
 static SemaphoreHandle_t empty_slots;
 static SemaphoreHandle_t filled_slots;
 static SemaphoreHandle_t mutex;
 
-//*****************************************************************************
-// Producer: write a given number of times to shared buffer
 void producer(void *parameters)
 {
-    int num = *(int *)parameters;
-    free(parameters);
-
-    // Signal to setup() that parameter has been read
+    int num = (int)(intptr_t)parameters; // Correct: pass value, not pointer
     xSemaphoreGive(bin_sem);
 
     for (int i = 0; i < num_writes; i++)
     {
         xSemaphoreTake(empty_slots, portMAX_DELAY);
         xSemaphoreTake(mutex, portMAX_DELAY);
-        buf[head] = num;
-        head = (head + 1) % BUF_SIZE;
+        buf[write_index] = num;
+        write_index = (write_index + 1) % BUF_SIZE;
         xSemaphoreGive(mutex);
         xSemaphoreGive(filled_slots);
     }
-
     vTaskDelete(NULL);
 }
 
-// Consumer: continuously read from shared buffer
 void consumer(void *parameters)
 {
     int val;
@@ -71,61 +47,52 @@ void consumer(void *parameters)
     {
         xSemaphoreTake(filled_slots, portMAX_DELAY);
         xSemaphoreTake(mutex, portMAX_DELAY);
-        val = buf[tail];
-        tail = (tail + 1) % BUF_SIZE;
+        val = buf[read_index];
+        read_index = (read_index + 1) % BUF_SIZE;
+        Serial.println(val);
         xSemaphoreGive(mutex);
         xSemaphoreGive(empty_slots);
-
-        // Print outside mutex for better performance
-        Serial.println(val);
     }
 }
 
-//*****************************************************************************
-// Main
-
 void setup()
 {
-    char task_name[20];
-
+    char task_name[12];
     Serial.begin(115200);
     vTaskDelay(1000 / portTICK_PERIOD_MS);
     Serial.println();
-    Serial.println("---FreeRTOS Semaphore Refined Solution---");
+    Serial.println("---FreeRTOS Semaphore CORRECTED Solution---");
 
-    // Create mutexes and semaphores before starting tasks
     bin_sem = xSemaphoreCreateBinary();
     mutex = xSemaphoreCreateMutex();
     empty_slots = xSemaphoreCreateCounting(BUF_SIZE, BUF_SIZE);
     filled_slots = xSemaphoreCreateCounting(BUF_SIZE, 0);
 
-    // Start producer tasks (wait for each to read argument)
     for (int i = 0; i < num_prod_tasks; i++)
     {
-        int *arg = (int *)malloc(sizeof(int));
-        *arg = i;
-        sprintf(task_name, "Producer %d", i);
-        xTaskCreatePinnedToCore(producer,
-                                task_name,
-                                1024,
-                                arg,
-                                1,
-                                NULL,
-                                app_cpu);
+        sprintf(task_name, "Producer %i", i);
+        xTaskCreatePinnedToCore(
+            producer,
+            task_name,
+            1024,
+            (void *)(intptr_t)i, // Pass value directly
+            1,
+            NULL,
+            app_cpu);
         xSemaphoreTake(bin_sem, portMAX_DELAY);
     }
 
-    // Start consumer tasks
     for (int i = 0; i < num_cons_tasks; i++)
     {
-        sprintf(task_name, "Consumer %d", i);
-        xTaskCreatePinnedToCore(consumer,
-                                task_name,
-                                1024,
-                                NULL,
-                                1,
-                                NULL,
-                                app_cpu);
+        sprintf(task_name, "Consumer %i", i);
+        xTaskCreatePinnedToCore(
+            consumer,
+            task_name,
+            1024,
+            NULL,
+            1,
+            NULL,
+            app_cpu);
     }
 
     Serial.println("All tasks created");
--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,5 @@
-/**
- * FreeRTOS Counting Semaphore Challenge
- *
- * Challenge: use a mutex and counting semaphores to protect the shared buffer
- * so that each number (0 throguh 4) is printed exactly 3 times to the Serial
- * monitor (in any order). Do not use queues to do this!
- *
- * Hint: you will need 2 counting semaphores in addition to the mutex, one for
- * remembering number of filled slots in the buffer and another for
- * remembering the number of empty slots in the buffer.
- *
- * Date: January 24, 2021
- * Author: Shawn Hymel
- * License: 0BSD
- **/
-
 #include <Arduino.h>
+#include <stdint.h>
 
 #if CONFIG_FREERTOS_UNICORE
 static const BaseType_t app_cpu = 0;
@@ -22,134 +7,98 @@ static const BaseType_t app_cpu = 0;
 static const BaseType_t app_cpu = 1;
 #endif
 
-// Settings
 enum
 {
     BUF_SIZE = 5
-}; // Size of buffer array
-static const int num_prod_tasks = 5; // Number of producer tasks
-static const int num_cons_tasks = 2; // Number of consumer tasks
-static const int num_writes = 3;     // Num times each producer writes to buf
+};
+static const int num_prod_tasks = 5;
+static const int num_cons_tasks = 2;
+static const int num_writes = 3;
 
-// Globals
-static int buf[BUF_SIZE];              // Shared buffer
-static int head = 0;                   // Writing index to buffer
-static int tail = 0;                   // Reading index to buffer
-static SemaphoreHandle_t bin_sem;      // Waits for parameter to be read
-static SemaphoreHandle_t empty_slots;  // Counts empty slots in buffer
-static SemaphoreHandle_t filled_slots; // Counts filled slots in buffer
-static SemaphoreHandle_t mutex;        // Mutex for buffer access
+static int buf[BUF_SIZE];
+static int write_index = 0;
+static int read_index = 0;
+static SemaphoreHandle_t bin_sem;
+static SemaphoreHandle_t empty_slots;
+static SemaphoreHandle_t filled_slots;
+static SemaphoreHandle_t mutex;
 
-//*****************************************************************************
-// Tasks
-
-// Producer: write a given number of times to shared buffer
 void producer(void *parameters)
 {
-
-    // Copy the parameters into a local variable
-    int num = *(int *)parameters;
-    free(parameters);
-
-    // Release the binary semaphore, signaling parameter has been read
+    int num = (int)(intptr_t)parameters; // Correct: pass value, not pointer
     xSemaphoreGive(bin_sem);
 
-    // Fill shared buffer with task number
     for (int i = 0; i < num_writes; i++)
     {
-
-        // Wait for an empty slot in the buffer
         xSemaphoreTake(empty_slots, portMAX_DELAY);
-        // Critical section (accessing shared buffer)
         xSemaphoreTake(mutex, portMAX_DELAY);
-        buf[head] = num;
-        head = (head + 1) % BUF_SIZE;
+        buf[write_index] = num;
+        write_index = (write_index + 1) % BUF_SIZE;
         xSemaphoreGive(mutex);
-        // Signal that a new filled slot is available
         xSemaphoreGive(filled_slots);
     }
-
-    // Delete self task
     vTaskDelete(NULL);
 }
 
-// Consumer: continuously read from shared buffer
 void consumer(void *parameters)
 {
-
     int val;
-
-    // Read from buffer
     while (1)
     {
-
-        // Critical section (accessing shared buffer and Serial)
         xSemaphoreTake(filled_slots, portMAX_DELAY);
         xSemaphoreTake(mutex, portMAX_DELAY);
-        val = buf[tail];
-        tail = (tail + 1) % BUF_SIZE;
+        val = buf[read_index];
+        read_index = (read_index + 1) % BUF_SIZE;
         Serial.println(val);
         xSemaphoreGive(mutex);
         xSemaphoreGive(empty_slots);
     }
 }
 
-//*****************************************************************************
-// Main (runs as its own task with priority 1 on core 1)
-
 void setup()
 {
-
     char task_name[12];
-
-    // Configure Serial
     Serial.begin(115200);
-
-    // Wait a moment to start (so we don't miss Serial output)
     vTaskDelay(1000 / portTICK_PERIOD_MS);
     Serial.println();
-    Serial.println("---FreeRTOS Semaphore Alternate Solution---");
+    Serial.println("---FreeRTOS Semaphore CORRECTED Solution---");
 
-    // Create mutexes and semaphores before starting tasks
     bin_sem = xSemaphoreCreateBinary();
     mutex = xSemaphoreCreateMutex();
     empty_slots = xSemaphoreCreateCounting(BUF_SIZE, BUF_SIZE);
     filled_slots = xSemaphoreCreateCounting(BUF_SIZE, 0);
 
-    // Start producer tasks (wait for each to read argument)
     for (int i = 0; i < num_prod_tasks; i++)
     {
         sprintf(task_name, "Producer %i", i);
-        xTaskCreatePinnedToCore(producer,
-                                task_name,
-                                1024,
-                                (void *)&i,
-                                1,
-                                NULL,
-                                app_cpu);
+        xTaskCreatePinnedToCore(
+            producer,
+            task_name,
+            1024,
+            (void *)(intptr_t)i, // Pass value directly
+            1,
+            NULL,
+            app_cpu);
         xSemaphoreTake(bin_sem, portMAX_DELAY);
     }
 
-    // Start consumer tasks
     for (int i = 0; i < num_cons_tasks; i++)
     {
         sprintf(task_name, "Consumer %i", i);
-        xTaskCreatePinnedToCore(consumer,
-                                task_name,
-                                1024,
-                                NULL,
-                                1,
-                                NULL,
-                                app_cpu);
+        xTaskCreatePinnedToCore(
+            consumer,
+            task_name,
+            1024,
+            NULL,
+            1,
+            NULL,
+            app_cpu);
     }
 
-    // Notify that all tasks have been created
     Serial.println("All tasks created");
 }
 
 void loop()
 {
-
-    // Do nothing but allow yielding to lower-priority tasks
     vTaskDelay(1000 / portTICK_PERIOD_MS);
 }
