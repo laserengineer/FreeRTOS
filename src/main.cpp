a84410ea5c71af038c013e1b4fcea730217c3f4a
yang yu <yang.yu@chemelex.com>
2025-12-24 12:31:17 -0700

    office

--- src/main.cpp
+++ src/main.cpp
@@ -1,50 +1,61 @@
 #include <Arduino.h>
-#include <BoardConfig.h>
+#include <stdint.h>
+#include "BoardConfig.h"
 
-#define led_pin LED_BUILTIN // Use the built-in LED pin
-
-// ---- Configuration ----
-// Use only core 1 for demo purposes
 #if CONFIG_FREERTOS_UNICORE
 static const BaseType_t app_cpu = 0;
 #else
 static const BaseType_t app_cpu = 1;
 #endif
 
+// Settings
+static const TickType_t dim_delay = 5000 / portTICK_PERIOD_MS;
+
 // Pins (change this if your Arduino board does not have LED_BUILTIN defined)
-static SemaphoreHandle_t mutex = nullptr;
+static const int led_pin = LED_BUILTIN;
+
+// Globals
+static TimerHandle_t one_shot_timer = NULL;
+
+//*****************************************************************************
+// Callbacks
+
+// Turn off LED when timer expires
+void autoDimmerCallback(TimerHandle_t xTimer)
+{
+    digitalWrite(led_pin, LOW);
+}
 
 //*****************************************************************************
 // Tasks
 
-// Blink LED based on rate passed by parameter
-void blinkLED(void *parameters)
+// Echo things back to serial port, turn on LED when while entering input
+void doCLI(void *parameters)
 {
 
-    // Copy the parameter into a local variable
-    mutex = xSemaphoreCreateMutex();
-    int num = *(int *)parameters;
+    char c;
 
-    // Release the mutex
-    xSemaphoreGive(mutex);
-
-    // Print the parameter
-    Serial.print("Received: ");
-    Serial.println(num);
-
-    // Configure the LED pin
+    // Configure LED pin
     pinMode(led_pin, OUTPUT);
 
-    // Blink forever and ever
     while (1)
     {
-        Serial.print(pcTaskGetName(NULL));
-        Serial.print(" : ");
-        Serial.println(num);
-        digitalWrite(led_pin, HIGH);
-        vTaskDelay(num / portTICK_PERIOD_MS);
-        digitalWrite(led_pin, LOW);
-        vTaskDelay(num / portTICK_PERIOD_MS);
+
+        // See if there are things in the input serial buffer
+        if (Serial.available() > 0)
+        {
+
+            // If so, echo everything back to the serial port
+            c = Serial.read();
+            Serial.print(c);
+
+            // Turn on the LED
+            digitalWrite(led_pin, HIGH);
+
+            // Start timer (if timer is already running, this will act as
+            // xTimerReset() instead)
+            xTimerStart(one_shot_timer, portMAX_DELAY);
+        }
     }
 }
 
@@ -54,52 +65,36 @@ void blinkLED(void *parameters)
 void setup()
 {
 
-    long int delay_arg;
-
     // Configure Serial
     Serial.begin(115200);
 
     // Wait a moment to start (so we don't miss Serial output)
-    vTaskDelay(3000 / portTICK_PERIOD_MS);
-
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
     Serial.println();
-    Serial.println("---FreeRTOS Mutex Challenge---");
-    Serial.println("Enter a number for delay (milliseconds)");
+    Serial.println("---FreeRTOS Timer Solution---");
 
-    // Wait for input from Serial
-    while (Serial.available() <= 0)
-    {
-        vTaskDelay(10 / portTICK_PERIOD_MS);
-    }
+    // Create a one-shot timer
+    one_shot_timer = xTimerCreate(
+        "One-shot timer",    // Name of timer
+        dim_delay,           // Period of timer (in ticks)
+        pdFALSE,             // Auto-reload
+        (void *)0,           // Timer ID
+        autoDimmerCallback); // Callback function
 
-    // Read integer value
-    delay_arg = Serial.parseInt();
-    Serial.print("Sending: ");
-    Serial.println(delay_arg);
-
-    // Start task 1
-    xTaskCreatePinnedToCore(blinkLED,
-                            "Blink LED",
+    // Start command line interface (CLI) task
+    xTaskCreatePinnedToCore(doCLI,
+                            "Do CLI",
                             1024,
-                            (void *)&delay_arg,
+                            NULL,
                             1,
                             NULL,
                             app_cpu);
 
-    // Show that we accomplished our task of passing the stack-based argument
-
-    while (mutex == nullptr)
-    {
-        vTaskDelay(10 / portTICK_PERIOD_MS);
-    }
-
-    // xSemaphoreTake(mutex, portMAX_DELAY);
-    Serial.println("Done!");
+    // Delete "setup and loop" task
+    vTaskDelete(NULL);
 }
 
 void loop()
 {
-
-    // Do nothing but allow yielding to lower-priority tasks
-    vTaskDelay(1000 / portTICK_PERIOD_MS);
+    // Execution should never get here
 }
